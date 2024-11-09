abadf0420af4c632e5c6a548895e93f3e1c833e4
Charlie Root <root@eiragt.malavon.eu>
2024-11-09 23:06:17 +0100

    TEMP: enable DEBUG and fix incompatibilities in drivers ...

--- sys/dev/dwc/if_dwc.h
+++ sys/dev/dwc/if_dwc.h
@@ -76,7 +76,7 @@
 #define	 FLOW_CONTROL_TX	(1 << 1)	/* TX Flow control enable */
 #define	GMAC_VLAN_TAG		0x1C
 #define	VERSION			0x20
-#define	DEBUG			0x24
+//#define	DEBUG			0x24
 #define	LPI_CONTROL_STATUS	0x30
 #define	LPI_TIMERS_CONTROL	0x34
 #define	INTERRUPT_STATUS	0x38
