d378366be560c071cdbd90e5fbb9fb5c504b7531
Benny Goemans <benny.goemans@wacadu.com>
2024-11-10 19:50:59 +0100

    D46714: syscon stuff

--- sys/arm/ti/ti_prcm.c
+++ sys/arm/ti/ti_prcm.c
@@ -36,14 +36,9 @@
 #include <sys/bus.h>
 #include <sys/kernel.h>
 #include <sys/module.h>
-#include <sys/malloc.h>
+#include <sys/mutex.h>
 #include <sys/rman.h>
-#include <sys/timeet.h>
-#include <sys/timetc.h>
-#include <sys/watchdog.h>
 #include <machine/bus.h>
-#include <machine/cpu.h>
-#include <machine/intr.h>
 
 #include <arm/ti/ti_cpuid.h>
 #include <arm/ti/ti_prcm.h>
@@ -55,6 +50,11 @@
 #include <dev/ofw/ofw_bus.h>
 #include <dev/ofw/ofw_bus_subr.h>
 
+#include <dev/extres/clk/clk.h>
+#include <dev/extres/syscon/syscon.h>
+#include <dev/extres/syscon/syscon_generic.h>
+
+#include "syscon_if.h"
 #include "clkdev_if.h"
 
 #if 0
@@ -63,17 +63,7 @@
 #define DPRINTF(dev, msg...)
 #endif
 
-struct ti_prcm_softc {
-	struct simplebus_softc  sc_simplebus;
-	device_t		dev;
-	struct resource *	mem_res;
-	bus_space_tag_t		bst;
-	bus_space_handle_t	bsh;
-	int			attach_done;
-	struct mtx		mtx;
-};
-
-static struct ti_prcm_softc *ti_prcm_sc = NULL;
+static device_t ti_prcm_dev;
 static void omap4_prcm_reset(void);
 static void am335x_prcm_reset(void);
 
@@ -98,25 +88,25 @@ static void am335x_prcm_reset(void);
 #define TI_PRCM_END		0
 
 static struct ofw_compat_data compat_data[] = {
-	{ "ti,am3-prcm",		TI_AM3_PRCM },
-	{ "ti,am4-prcm",		TI_AM4_PRCM },
-	{ "ti,omap2-prcm",		TI_OMAP2_PRCM },
-	{ "ti,omap3-prm",		TI_OMAP3_PRM },
-	{ "ti,omap3-cm",		TI_OMAP3_CM },
-	{ "ti,omap4-cm1",		TI_OMAP4_CM1 },
-	{ "ti,omap4-prm",		TI_OMAP4_PRM },
-	{ "ti,omap4-cm2",		TI_OMAP4_CM2 },
-	{ "ti,omap4-scrm",		TI_OMAP4_SCRM },
-	{ "ti,omap5-prm",		TI_OMAP5_PRM },
-	{ "ti,omap5-cm-core-aon",	TI_OMAP5_CM_CORE_AON },
-	{ "ti,omap5-scrm",		TI_OMAP5_SCRM },
-	{ "ti,omap5-cm-core",		TI_OMAP5_CM_CORE },
-	{ "ti,dra7-prm",		TI_DRA7_PRM },
-	{ "ti,dra7-cm-core-aon",	TI_DRA7_CM_CORE_AON },
-	{ "ti,dra7-cm-core",		TI_DRA7_CM_CORE },
-	{ "ti,dm814-prcm",		TI_DM814_PRCM },
-	{ "ti,dm816-prcm",		TI_DM816_PRCM },
-	{ NULL,				TI_PRCM_END}
+	{ "ti,am3-prcm",	TI_AM3_PRCM },
+	{ "ti,am4-prcm",	TI_AM4_PRCM },
+	{ "ti,omap2-prcm",	TI_OMAP2_PRCM },
+	{ "ti,omap3-prm",	TI_OMAP3_PRM },
+	{ "ti,omap3-cm",	TI_OMAP3_CM },
+	{ "ti,omap4-cm1",	TI_OMAP4_CM1 },
+	{ "ti,omap4-prm",	TI_OMAP4_PRM },
+	{ "ti,omap4-cm2",	TI_OMAP4_CM2 },
+	{ "ti,omap4-scrm",	TI_OMAP4_SCRM },
+	{ "ti,omap5-prm",	TI_OMAP5_PRM },
+	{ "ti,omap5-cm-core-aon", TI_OMAP5_CM_CORE_AON },
+	{ "ti,omap5-scrm",	TI_OMAP5_SCRM },
+	{ "ti,omap5-cm-core",	TI_OMAP5_CM_CORE },
+	{ "ti,dra7-prm",	TI_DRA7_PRM },
+	{ "ti,dra7-cm-core-aon", TI_DRA7_CM_CORE_AON },
+	{ "ti,dra7-cm-core",	TI_DRA7_CM_CORE },
+	{ "ti,dm814-prcm",	TI_DM814_PRCM },
+	{ "ti,dm816-prcm",	TI_DM816_PRCM },
+	{ NULL,			TI_PRCM_END}
 };
 
 static int
@@ -125,56 +115,15 @@ ti_prcm_probe(device_t dev)
 	if (!ofw_bus_status_okay(dev))
 		return (ENXIO);
 
-	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0) {
+	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
-	}
 
 	device_set_desc(dev, "TI Power and Clock Management");
-	return(BUS_PROBE_DEFAULT);
-}
+	if (bootverbose == 0)
+		device_quiet(dev);
 
-static int
-ti_prcm_attach(device_t dev)
-{
-	struct ti_prcm_softc *sc;
-	phandle_t node, child;
-	int rid;
-
-	sc = device_get_softc(dev);
-	sc->dev = dev;
-
-	node = ofw_bus_get_node(sc->dev);
-	simplebus_init(sc->dev, node);
-
-	if (simplebus_fill_ranges(node, &sc->sc_simplebus) < 0) {
-		device_printf(sc->dev, "could not get ranges\n");
-		return (ENXIO);
-	}
-	if (sc->sc_simplebus.nranges == 0) {
-		device_printf(sc->dev, "nranges == 0\n");
-		return (ENXIO);
-	}
-
-	sc->mem_res = bus_alloc_resource(sc->dev, SYS_RES_MEMORY, &rid,
-		sc->sc_simplebus.ranges[0].host,
-		(sc->sc_simplebus.ranges[0].host +
-			sc->sc_simplebus.ranges[0].size - 1),
-		sc->sc_simplebus.ranges[0].size,
-		RF_ACTIVE | RF_SHAREABLE);
-
-	if (sc->mem_res == NULL) {
-		return (ENXIO);
-	}
-
-	sc->bst = rman_get_bustag(sc->mem_res);
-	sc->bsh = rman_get_bushandle(sc->mem_res);
-
-	mtx_init(&sc->mtx, device_get_nameunit(dev), NULL, MTX_DEF);
-
-	/* Fixme: for xxx_prcm_reset functions.
-	 * Get rid of global variables?
-	 */
-	ti_prcm_sc = sc;
+	/* Setup reset handler */
+	ti_prcm_dev = dev;
 
 	switch(ti_chip()) {
 #ifdef SOC_OMAP4
@@ -189,32 +138,26 @@ ti_prcm_attach(device_t dev)
 #endif
 	}
 
-	bus_generic_probe(sc->dev);
-	for (child = OF_child(node); child != 0; child = OF_peer(child)) {
-		simplebus_add_device(dev, child, 0, NULL, -1, NULL);
-	}
-
-	return (bus_generic_attach(sc->dev));
+	return (BUS_PROBE_DEFAULT);
 }
 
 int
 ti_prcm_write_4(device_t dev, bus_addr_t addr, uint32_t val)
 {
-	struct ti_prcm_softc *sc;
+	struct syscon_generic_softc *sc;
 
 	sc = device_get_softc(dev);
-	DPRINTF(sc->dev, "offset=%lx write %x\n", addr, val);
-	bus_space_write_4(sc->bst, sc->bsh, addr, val);
-	return (0);
+	DPRINTF(sc->dev, "ti_prcm_write_4: offset=%lx write %x\n", addr, val);
+	return (SYSCON_UNLOCKED_WRITE_4(sc->syscon, addr, val));
 }
+
 int
 ti_prcm_read_4(device_t dev, bus_addr_t addr, uint32_t *val)
 {
-	struct ti_prcm_softc *sc;
+	struct syscon_generic_softc *sc;
 
 	sc = device_get_softc(dev);
-
-	*val = bus_space_read_4(sc->bst, sc->bsh, addr);
+	*val = SYSCON_UNLOCKED_READ_4(sc->syscon, addr);
 	DPRINTF(sc->dev, "offset=%lx Read %x\n", addr, *val);
 	return (0);
 }
@@ -222,41 +165,53 @@ ti_prcm_read_4(device_t dev, bus_addr_t addr, uint32_t *val)
 int
 ti_prcm_modify_4(device_t dev, bus_addr_t addr, uint32_t clr, uint32_t set)
 {
-	struct ti_prcm_softc *sc;
-	uint32_t reg;
+	struct syscon_generic_softc *sc;
 
 	sc = device_get_softc(dev);
-
-	reg = bus_space_read_4(sc->bst, sc->bsh, addr);
-	reg &= ~clr;
-	reg |= set;
-	bus_space_write_4(sc->bst, sc->bsh, addr, reg);
-	DPRINTF(sc->dev, "offset=%lx reg: %x (clr %x set %x)\n", addr, reg, clr, set);
-
-	return (0);
+	DPRINTF(sc->dev, "ti_prcm_modify_4: addr %x clear_mask %x set_mask %x\n",
+		 addr, clr, set);
+	return (SYSCON_UNLOCKED_MODIFY_4(sc->syscon, addr, clr,
+		set));
 }
 
 void
 ti_prcm_device_lock(device_t dev)
 {
-	struct ti_prcm_softc *sc;
+	struct syscon_generic_softc *sc;
 
 	sc = device_get_softc(dev);
-	mtx_lock(&sc->mtx);
+	DPRINTF(sc->dev, "ti_prcm_device_lock\n");
+	SYSCON_DEVICE_LOCK(sc->syscon->pdev);
 }
 
 void
 ti_prcm_device_unlock(device_t dev)
 {
-	struct ti_prcm_softc *sc;
+	struct syscon_generic_softc *sc;
 
 	sc = device_get_softc(dev);
-	mtx_unlock(&sc->mtx);
+	DPRINTF(sc->dev, "ti_prcm_device_unlock\n");
+	SYSCON_DEVICE_UNLOCK(sc->syscon->pdev);
+}
+
+static int
+ti_prcm_get_handle(device_t dev, struct syscon **syscon)
+{
+	struct syscon_generic_softc *sc;
+
+	sc = device_get_softc(dev);
+	*syscon = sc->syscon;
+	if (*syscon == NULL)
+		return (ENODEV);
+	return (0);
 }
 
 static device_method_t ti_prcm_methods[] = {
+	/* Device interface */
 	DEVMETHOD(device_probe,		ti_prcm_probe),
-	DEVMETHOD(device_attach,	ti_prcm_attach),
+
+	/* Syscon */
+	DEVMETHOD(syscon_get_handle, ti_prcm_get_handle),
 
 	/* clkdev interface */
 	DEVMETHOD(clkdev_write_4,	ti_prcm_write_4),
@@ -269,13 +224,13 @@ static device_method_t ti_prcm_methods[] = {
 };
 
 DEFINE_CLASS_1(ti_prcm, ti_prcm_driver, ti_prcm_methods,
-    sizeof(struct ti_prcm_softc), simplebus_driver);
+	sizeof(struct syscon_generic_softc), syscon_generic_driver);
 
-EARLY_DRIVER_MODULE(ti_prcm, ofwbus, ti_prcm_driver, 0, 0, BUS_PASS_BUS);
 EARLY_DRIVER_MODULE(ti_prcm, simplebus, ti_prcm_driver, 0, 0,
     BUS_PASS_BUS + BUS_PASS_ORDER_MIDDLE);
+
 MODULE_VERSION(ti_prcm, 1);
-MODULE_DEPEND(ti_prcm, ti_scm, 1, 1, 1);
+MODULE_DEPEND(ti_prcm, syscon_generic_dev, 1, 1, 1);
 
 /* From sys/arm/ti/am335x/am335x_prcm.c
  * Copyright (c) 2012 Damjan Marion <dmarion@Freebsd.org>
@@ -286,7 +241,9 @@ MODULE_DEPEND(ti_prcm, ti_scm, 1, 1, 1);
 static void
 am335x_prcm_reset(void)
 {
-	ti_prcm_write_4(ti_prcm_sc->dev, AM335x_PRM_RSTCTRL, (1<<1));
+	ti_prcm_device_lock(ti_prcm_dev);
+	ti_prcm_write_4(ti_prcm_dev, AM335x_PRM_RSTCTRL, (1<<1));
+	ti_prcm_device_unlock(ti_prcm_dev);
 }
 
 /* FIXME: Is this correct - or should the license part be ontop? */
@@ -330,8 +287,10 @@ omap4_prcm_reset(void)
 {
 	uint32_t reg;
 
-	ti_prcm_read_4(ti_prcm_sc->dev, PRM_RSTCTRL, &reg);
+	ti_prcm_device_lock(ti_prcm_dev);
+	ti_prcm_read_4(ti_prcm_dev, PRM_RSTCTRL, &reg);
 	reg = reg | PRM_RSTCTRL_RESET;
-	ti_prcm_write_4(ti_prcm_sc->dev, PRM_RSTCTRL, reg);
-	ti_prcm_read_4(ti_prcm_sc->dev, PRM_RSTCTRL, &reg);
+	ti_prcm_write_4(ti_prcm_dev, PRM_RSTCTRL, reg);
+	ti_prcm_read_4(ti_prcm_dev, PRM_RSTCTRL, &reg);
+	ti_prcm_device_unlock(ti_prcm_dev);
 }
